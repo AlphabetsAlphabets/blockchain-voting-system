cb933501888d09b7abc521437909945cae608835
Tamim mustapha <tamimmustapha@Tamims-MacBook-Pro.local>
2025-03-20 11:44:48 +0800

    Integratio without the front end

--- ignition/modules/Deploy.ts
+++
@@ -1,6 +0,0 @@
-import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
-
-export default buildModule("DeployModule", (m) => {
-  const contract = m.contract("Incrementer");
-  return { contract };
-});
