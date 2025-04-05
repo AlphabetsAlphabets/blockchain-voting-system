4d5c2162e173dad61a93c0bdbe1ce6f06d97b120
Alphabets, Alphabets <yapjiahong-p@proton.me>
2025-04-05 16:45:56 +0800

    refactor: moved run.py code into if guard

--- BCBackendFlask/run.py
+++ BCBackendFlask/run.py
@@ -1,8 +1,8 @@
 from app import create_app
 
-# Initialize Flask app
-app = create_app()
 
 # Run the app
 if __name__ == "__main__":
+    # Initialize Flask app
+    app = create_app()
     app.run(debug=True)
