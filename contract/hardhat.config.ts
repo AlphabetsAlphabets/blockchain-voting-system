911d69be3530aabab07e90e0af9e5a0e498537f5
Marcel Putra <marcelino.ares.putra@gmail.com>
2025-04-12 22:26:59 +0800

    code: modified hardhat config

--- contract/hardhat.config.ts
+++ contract/hardhat.config.ts
@@ -1,14 +1,35 @@
-import type { HardhatUserConfig } from 'hardhat/config';
-import '@nomicfoundation/hardhat-toolbox-viem';
+import { HardhatUserConfig } from "hardhat/types";
+import "@nomicfoundation/hardhat-toolbox";
+import dotenv from "dotenv";
+import "@nomiclabs/hardhat-etherscan";
+
+dotenv.config();
 
 const config: HardhatUserConfig = {
-  solidity: '0.8.28',
+  solidity: "0.8.19",
   networks: {
-    localhost: {
-      chainId: 31337,
-      url: 'http://127.0.0.1:8545',
-    },
+    scrollSepolia: {
+      url: process.env.SCROLL_SEPOLIA_URL || "",
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+    }
   },
-};
+
+  // for test verification (may become deprecated down the line)
+  etherscan: {
+    apiKey: {
+      scrollSepolia: process.env.SCROLLSCAN_API_KEY || "",
+    },
+    customChains: [
+      {
+        network: "scrollSepolia",
+        chainId: 534351,
+        urls: {
+          apiURL: "https://api-sepolia.scrollscan.com/api",
+          browserURL: "https://sepolia.scrollscan.com"
+        }
+      }
+    ]
+  }
+}
 
 export default config;
