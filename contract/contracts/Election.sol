fc33d78f5c06730a5f7b5fdaca0fc63fa229ad87
Marcel Putra <marcelino.ares.putra@gmail.com>
2025-04-12 18:38:38 +0800

    code: refactor Election.sol - removed voter struct, as I shifted functionality to the vote function itself. - rewrote vote functions - implemented function modifiers - rewrote constructor

--- contract/contracts/Election.sol
+++ contract/contracts/Election.sol
@@ -1,69 +1,103 @@
+// https://spdx.org/licenses/ , not important but should be considered in documentation
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19
 
 struct Proposal {
-    bytes32 name;
+    string name;
     uint votes;
-}
-
-struct Voter {
-    address wallet;
-    bool voted;
 }
 
 contract Election {
     address public owner;
     bool public ended;
 
-    // string is the name of the thing to vote for.
-    mapping(string => Proposal) proposals;
-    mapping(address => bool) voters; 
+    mapping(string => Proposal) public proposals;
+    mapping(address => bool) public voterRegistry;  // for tracking who can vote
+    mapping(address => bool) public hasVoted;       // for tracking who has voted
 
-    uint start_date;
-    uint end_date;   
+    uint public startTime;
+    uint public endTime;
+    string[] public proposalNames; // to track names of proposals (redundant?)
 
-    constructor(string[] items, Voter[] allowed_voters, string start, string end) {
-        for (uint256 i = 0; i < items.length; i++) {
-            proposals[items[i]] = Proposal {
-                name: items[i],
-                votes: 0,
-            }
-        }
-
-        for (uint i = 0; i < allowed_voters.length; i++) {
-            voters[allowed_voters[i]] = false;
-        }
-
-        // Will need to convert start and end into a unix time stamp
-        // then save it to start_date and end_date.
-        owner = msg.sender;
+    // modifiers are used to change the behaviours of functions in a declarative way
+    // allows us to add preconditions, postconditions, or other logic that must be executed before or after a function call
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only owner can call this");
+        _;
     }
 
-    // 0 = ok, 1 = failure, 2 = election ended
-    function vote(address person, string proposal) public returns (uint status) {
-        if (ended) {
-            return 2;
+    modifier onlyDuringVotingPeriod() {
+        require(block.timestamp >= startTime && block.timestamp <= endTime, "Not during voting period");
+        require(!ended, "Election has ended");
+        _;
+    }
+
+    constructor(string[] memory items, address[] memory allowedVoters, uint _startTime, uint _endTime) {
+        require(_startTime < _endTime, "Start time must be before end time");
+        require(items.length > 0, "At least one proposal required"); // set minimum proposals here
+
+        owner = msg.sender;
+        startTime = _startTime;
+        endTime = _endTime;
+
+        for (uint256 i = 0; i < items.length; i++) {
+            proposals[items[i]] = Proposal({
+                name: items[i],
+                votes: 0
+            });
+            proposalNames.push(items[i]);
         }
 
-        Voter voter = voters[person];
-        if (voter.voted) {
-            return 1;
+        for (uint i = 0; i < allowedVoters.length; i++) {
+            voterRegistry[allowedVoters[i]] = true;
         }
+    }
+
+    // actual use-case functions below
+
+    // note, "external" means it can only be called from outside the contract
+    // this is good practice if you want to add additional functions
+
+    // "msg.sender" refers to the address of the account that called the function
+    // NEVER USE "tx.origin" FOR AUTH OPERATIONS, IT IS VULNERABLE TO PHISHING ATTACKS
+    
+    // function to cast a vote
+    function vote(string memory proposal) external onlyDuringVotingPeriod returns(uint status) {
+        
+        // I got rid of the voter struct as verification can be done inside the vote function itself
+        // implemented a voter registry system against the current double voting issue for now, this needs revision later
+
+        require(voterRegistry[msg.sender], "Not registered to vote");
+        require(!hasVoted[msg.sender], "Already voted");
+        require(bytes(proposals[proposal].name).length != 0, "Invalid proposal");
 
         proposals[proposal].votes += 1;
-        return 0;
+        hasVoted[msg.sender] = true;
+
+        return 0; // success
     }
 
-    function end_vote(address person) public returns (uint status) {
-        if (person == owner) {
-            ended = true;
+    function endVote() external onlyOwner {
+        require(block.timestamp > endTime || !ended, "Election already ended or not finished");
+        ended = true;
+    }
+
+    function getProposalVotes(string memory proposal) public view returns (uint) {
+        return proposals[proposal].votes;
+    }
+
+    function getAllProposals() public view returns (Proposal[] memory) {
+        Proposal[] memory allProposals = new Proposal[](proposalNames.length);
+        for (uint i = 0; i < proposalNames.length; i++) {
+            allProposals[i] = proposals[proposalNames[i]];
         }
+        return allProposals;
     }
 
-    function end_vote_automatically() public returns (uint status) {
-        // TODO: Will need to call from the outside.
-
-
-        return end_vote(owner);
+    function checkElectionStatus() public view returns (string memory) {
+        if (ended) return "Election ended";
+        if (block.timestamp < startTime) return "Election not started";
+        if (block.timestamp > endTime) return "Voting period over (ready to end)";
+        return "Voting in progress";
     }
-
 }
