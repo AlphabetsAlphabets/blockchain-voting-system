e4ddc734843918132a462967805f2608eeb6e00d
Marcel Putra <marcelino.ares.putra@gmail.com>
2025-04-12 22:54:46 +0800

    code: created deployment script - created basic test deployment script - also removed etherscan cuz why not (don't want to handle it rn)

--- contract/hardhat.config.ts
+++ contract/hardhat.config.ts
@@ -2,6 +2,7 @@ import { HardhatUserConfig } from "hardhat/types";
 import "@nomicfoundation/hardhat-toolbox";
 import dotenv from "dotenv";
 import "@nomiclabs/hardhat-etherscan";
+import "@nomiclabs/hardhat-ethers";
 
 dotenv.config();
 
@@ -13,23 +14,6 @@ const config: HardhatUserConfig = {
       accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
     }
   },
-
-  // for test verification (may become deprecated down the line)
-  etherscan: {
-    apiKey: {
-      scrollSepolia: process.env.SCROLLSCAN_API_KEY || "",
-    },
-    customChains: [
-      {
-        network: "scrollSepolia",
-        chainId: 534351,
-        urls: {
-          apiURL: "https://api-sepolia.scrollscan.com/api",
-          browserURL: "https://sepolia.scrollscan.com"
-        }
-      }
-    ]
-  }
 }
 
 export default config;
