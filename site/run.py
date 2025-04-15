449029143ed91e563acdf3dcccc665b97113cb7c
Alphabets, Alphabets <yapjiahong-p@proton.me>
2025-04-15 16:00:06 +0800

    wip: generalising contract interface
    Works wel for just one contract but I'd like to make it more generic so that any contract can be called from it.

--- site/run.py
+++ site/run.py
@@ -1,8 +1,13 @@
 from app import create_app
+from web3_utils import ContractHelper
 
+contract_helper = None
 
 # Run the app
 if __name__ == "__main__":
+    # Setup connection to blockchain
+    contract_helper = ContractHelper()
+
     # Initialize Flask app
     app = create_app()
     app.run(debug=True)
